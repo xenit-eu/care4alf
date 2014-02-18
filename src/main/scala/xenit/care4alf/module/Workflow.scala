082b59d2d58ae80625280c94f67f56fe89f6f46c
Laurent Van der Linden <laurent.vanderlinden@xenit.eu>
2014-02-18 09:52:00 +0100

    delete definition files

--- src/main/scala/xenit/care4alf/module/Workflow.scala
+++ src/main/scala/xenit/care4alf/module/Workflow.scala
@@ -9,6 +9,9 @@ import scala.collection.JavaConversions._
 import org.springframework.stereotype.Component
 import org.alfresco.service.cmr.workflow.{WorkflowInstance, WorkflowService}
 import com.typesafe.scalalogging.slf4j.Logging
+import org.alfresco.service.cmr.search.SearchService
+import org.alfresco.service.cmr.repository.{NodeService, StoreRef}
+import org.alfresco.model.ContentModel
 
 /**
  * @author Laurent Van der Linden
@@ -18,6 +21,8 @@ import com.typesafe.scalalogging.slf4j.Logging
 class Workflow @Autowired() (namespaceService: NamespaceService) extends Json with Logging
 {
     @Autowired var workflowService: WorkflowService = null
+    @Autowired var searchService: SearchService = null
+    @Autowired var nodeService: NodeService = null
 
     @Uri(defaultFormat = "json", method = HttpMethod.GET)
     def list(@Attribute jsonHelper: JsonHelper) {
@@ -38,10 +43,21 @@ class Workflow @Autowired() (namespaceService: NamespaceService) extends Json wi
     def deleteNameless(@UriVariable workflowId: String) {
         for (list <- Seq(workflowService.getCompletedWorkflows,workflowService.getActiveWorkflows)) {
             if (list != null) {
-                list.filter(_.getDefinition.getId == workflowId)
+                list.filter((wf) => wf.getDefinition != null && wf.getDefinition.getId == workflowId)
                     .foreach((workflow) => workflowService.cancelWorkflow(workflow.getId))
             }
         }
-        workflowService.undeployDefinition(workflowId)
+
+        val name = workflowService.getDefinitionById(workflowId).getName
+        val query = "@bpm\\:definitionName:\"" + name + "\""
+        val definitionFiles = searchService.query(StoreRef.STORE_REF_WORKSPACE_SPACESSTORE, SearchService.LANGUAGE_LUCENE, query)
+        if (definitionFiles.length() > 0) {
+            for (defFile <- definitionFiles) {
+                nodeService.addAspect(defFile.getNodeRef, ContentModel.ASPECT_TEMPORARY, null)
+                nodeService.deleteNode(defFile.getNodeRef)
+            }
+        } else {
+            workflowService.undeployDefinition(workflowId)
+        }
     }
 }
