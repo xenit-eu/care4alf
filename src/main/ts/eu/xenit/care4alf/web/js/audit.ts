f67da1eae858380da6d93ebf0586bd4d96382d85
Willem Van den Eynde <wvandeneynde@xenit.eu>
2015-09-22 10:57:38 +0200

    Increase audit limit from 100 to 1000

--- src/main/ts/eu/xenit/care4alf/web/js/audit.ts
+++ src/main/ts/eu/xenit/care4alf/web/js/audit.ts
@@ -10,14 +10,14 @@ care4alf.controller('audit', function($scope, $http) {
     $scope.entries=[];
     $scope.load = function(app){
         console.log("loading app: " + app.path);
-        $http.get("/alfresco/service/api/audit/query" + app.path + "?verbose=true").success(function(data){
+        $http.get("/alfresco/service/api/audit/query" + app.path + "?verbose=true&limit=1000").success(function(data){
             $scope.entries = data.entries;
         });
     };
 
     $scope.query = function(app,key, value){
         console.log("Query: " + value);
-        $http.get("/alfresco/s/api/audit/query"+app.path+key+"?verbose=true&value="+value).success(function(data){
+        $http.get("/alfresco/s/api/audit/query"+app.path+key+"?verbose=true&limit=1000&value="+value).success(function(data){
             $scope.entries = data.entries;
         });
     };
