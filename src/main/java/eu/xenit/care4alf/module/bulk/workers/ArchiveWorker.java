a465c097975531a91d3a2dcf1608d7a78f8be146
Michiel Huygen <michiel.huygen@xenit.eu>
2015-10-20 17:25:14 +0200

    Added the new worker annotation to all the existing bulk workers

--- src/main/java/eu/xenit/care4alf/module/bulk/workers/ArchiveWorker.java
+++ src/main/java/eu/xenit/care4alf/module/bulk/workers/ArchiveWorker.java
@@ -1,8 +1,10 @@
 package eu.xenit.care4alf.module.bulk.workers;
 
 import eu.xenit.care4alf.module.bulk.AbstractWorker;
+import eu.xenit.care4alf.module.bulk.Worker;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.json.JSONObject;
+import org.springframework.stereotype.Component;
 
 //import org.apache.commons.logging.Log;
 //import org.apache.commons.logging.LogFactory;
@@ -11,8 +13,14 @@ import org.json.JSONObject;
 /**
  * Created by willem on 3/12/15.
  */
+@Component
+@Worker(action = "archive")
 public class ArchiveWorker extends AbstractWorker {
 //    private static Log logger = LogFactory.getLog(DeleteWorker.class);
+
+    public ArchiveWorker(){
+        super(null) ;
+    }
 
     public ArchiveWorker(JSONObject parameters)
     {
