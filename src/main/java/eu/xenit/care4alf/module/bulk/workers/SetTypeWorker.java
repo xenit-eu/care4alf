6878e53c2af5876d21053d5ad3ba6b37fca28167
Michiel Huygen <michiel.huygen@xenit.eu>
2015-10-20 17:21:21 +0200

    Moved workers to separate package

--- src/main/java/eu/xenit/care4alf/module/bulk/workers/SetTypeWorker.java
+++ src/main/java/eu/xenit/care4alf/module/bulk/workers/SetTypeWorker.java
@@ -1,5 +1,6 @@
-package eu.xenit.care4alf.module.bulk;
+package eu.xenit.care4alf.module.bulk.workers;
 
+import eu.xenit.care4alf.module.bulk.AbstractWorker;
 import org.alfresco.service.cmr.repository.NodeRef;
 import org.alfresco.service.namespace.QName;
 import org.json.JSONObject;
