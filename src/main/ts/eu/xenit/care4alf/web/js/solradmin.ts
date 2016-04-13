a7219c652e551df82da13210a7b6cf40a8cb82fd
Willem Van den Eynde <wvandeneynde@xenit.eu>
2016-04-13 23:27:29 +0200

    Added more solr links

--- src/main/ts/eu/xenit/care4alf/web/js/solradmin.ts
+++ src/main/ts/eu/xenit/care4alf/web/js/solradmin.ts
@@ -4,4 +4,14 @@ care4alf.controller('solradmin', ($scope,$http: ng.IHttpService) => {
     $http.get('/alfresco/s/xenit/care4alf/solr/proxy/alfresco/admin/luke?numTerms=0').success((data:any) => {
         $scope.fields = Object.keys(data.fields);
     });
+
+    $scope.result="";
+    $scope.action=(operation)=>{
+        var url = "/alfresco/s/xenit/care4alf/solr/proxy/admin/cores?wt=json&action=" + operation;
+        if($scope.nodeid) url += "&nodeid=" + $scope.nodeid;
+        console.log(url);
+        $http.get(url).success((data:any) => {
+            $scope.result = data;
+        });
+    }
 });
