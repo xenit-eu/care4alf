bd1806c112b816851b4ee82a10948fa5f9a9a9f3
Willem Van den Eynde <wvandeneynde@xenit.eu>
2016-05-25 16:50:33 +0200

    Fix not indexed documents becauseof  timeouts

--- src/main/ts/eu/xenit/care4alf/web/js/solradmin.ts
+++ src/main/ts/eu/xenit/care4alf/web/js/solradmin.ts
@@ -1,16 +1,24 @@
 /// <reference path="care4alf.ts" />
 
 care4alf.controller('solradmin', ($scope,$http: ng.IHttpService) => {
-    $http.get('/alfresco/s/xenit/care4alf/solr/proxy/alfresco/admin/luke?numTerms=0').success((data:any) => {
+    $http.get('solr/proxy/alfresco/admin/luke?numTerms=0').success((data:any) => {
         $scope.fields = Object.keys(data.fields);
     });
 
     $scope.result="";
     $scope.action=(operation)=>{
-        var url = "/alfresco/s/xenit/care4alf/solr/proxy/admin/cores?wt=json&action=" + operation;
+        var url = "solr/proxy/admin/cores?wt=json&action=" + operation;
         if($scope.nodeid) url += "&nodeid=" + $scope.nodeid;
         console.log(url);
         $http.get(url).success((data:any) => {
+            $scope.result = data;
+        });
+    };
+    $scope.errors=[{exception:"Read timed out",count:0}];//TODO: dynamic + numFound
+    $scope.errorFix=(exception,operation)=>{
+        var url = "solr/errors/nodes/fix/"+exception;
+        console.log(url);
+        $http.post(url,{}).success((data:any) => {
             $scope.result = data;
         });
     }
