c01e8fa754591be6fd1852907851bc404382daff
Willem Van den Eynde <wvandeneynde@xenit.eu>
2016-06-28 14:28:28 +0200

    Overview of big transactions

--- src/main/ts/eu/xenit/care4alf/web/js/monitoring.ts
+++ src/main/ts/eu/xenit/care4alf/web/js/monitoring.ts
@@ -8,6 +8,9 @@ care4alf.controller('monitoring', ($scope,$http: ng.IHttpService) => {
             },
         "solr.errors":{
             "description":'Number of Solr errors. Get an <a href="solr/errors/nodes">overview</a> of the nodes not indexed and try to <a href="#/solradmin">fix</a> them accordingly.'
+        },
+        "solr.lag.nodes":{
+            "description": 'Number of nodes to index. If this is too high, check the <a href="solr/transactions">transaction</a> sizes'
         }
     };
     $http.get('monitoring/vars').success((result:any) => {
