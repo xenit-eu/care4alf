5a23238e4ec6970453d003babaf3fef94c0af8ec
Thomas Straetmans <Thomas.Straetmans@xenit.eu>
2016-09-14 17:41:47 +0200

    Added extra monitoring for Model errors.

--- src/main/java/eu/xenit/care4alf/Monitoring.java
+++ src/main/java/eu/xenit/care4alf/Monitoring.java
@@ -19,6 +19,8 @@ import org.apache.commons.codec.EncoderException;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.webscripts.WebScriptResponse;
 import org.springframework.stereotype.Component;
@@ -54,6 +56,9 @@ public class Monitoring {
     @Autowired
     private Clustering clustering;
 
+    private final Logger logger = LoggerFactory.getLogger(Monitoring.class);
+
+
     @Uri("/xenit/care4alf/monitoring")
     public void monitoring(final WebScriptResponse res) throws IOException, JSONException {
         Descriptor descriptor = getDescriptor();
@@ -61,24 +66,24 @@ public class Monitoring {
         final JSONWriter jsonRes = new JSONWriter(res.getWriter());
         jsonRes.object();
         jsonRes.key("data");
-            jsonRes.object();
-                jsonRes.key("edition").value(descriptor.getLicenseMode());
-                jsonRes.key("version").value(descriptor.getVersion());
-                jsonRes.key("schema").value(descriptor.getSchema());
-            jsonRes.endObject();
+        jsonRes.object();
+        jsonRes.key("edition").value(descriptor.getLicenseMode());
+        jsonRes.key("version").value(descriptor.getVersion());
+        jsonRes.key("schema").value(descriptor.getSchema());
+        jsonRes.endObject();
         jsonRes.endObject();
     }
 
-    private long dbCheck(){
+    private long dbCheck() {
         try {
             this.getDescriptor();
-        }catch(Exception e){
+        } catch (Exception e) {
             return -1;
         }
         return 1;
     }
 
-    private Descriptor getDescriptor(){
+    private Descriptor getDescriptor() {
         return currentRepoDescriptorDAO.getDescriptor();
     }
 
@@ -88,17 +93,17 @@ public class Monitoring {
         res.getWriter().write("OK");
     }
 
-    @Uri(value="/xenit/care4alf/monitoring/solr/errors",defaultFormat = "text")
+    @Uri(value = "/xenit/care4alf/monitoring/solr/errors", defaultFormat = "text")
     public void getSolrErrors(WebScriptResponse res) throws IOException, JSONException, EncoderException {
         res.getWriter().write(Long.toString(this.solrAdmin.getSolrErrors()));
     }
 
-    @Uri(value="/xenit/care4alf/monitoring/solr/lag",defaultFormat = "text")
+    @Uri(value = "/xenit/care4alf/monitoring/solr/lag", defaultFormat = "text")
     public void getSolrLags(WebScriptResponse res) throws IOException, JSONException, EncoderException {
         res.getWriter().write(String.valueOf(this.solrAdmin.getSolrLag()));
     }
 
-    private long getResidualProperties(String filter){
+    private long getResidualProperties(String filter) {
         try {
             return this.properties.getResidualProperties(filter).size();
         } catch (SQLException e) {
@@ -107,15 +112,23 @@ public class Monitoring {
         }
     }
 
-    @Uri(value="/xenit/care4alf/monitoring/vars")
-    public Resolution getVars(WebScriptResponse response) throws IOException, JSONException {
-        final Map<String,Long> vars = new HashMap<String,Long>();
+    @Uri(value = "/xenit/care4alf/monitoring/vars")
+    public Resolution getVars(WebScriptResponse response) throws IOException, JSONException, EncoderException {
+        final Map<String, Long> vars = new HashMap<String, Long>();
         vars.put("db", this.dbCheck());
+        logger.debug("db done");
         vars.put("solr.errors", this.solrAdmin.getSolrErrors());
+        logger.debug("solr.errors done");
         vars.put("solr.lag", this.solrAdmin.getSolrLag());
+        logger.debug("solr.lag done");
         vars.put("solr.lag.nodes", this.solrAdmin.getNodesToIndex());
+        logger.debug("solr.lag.nodes done");
+        vars.put("solr.model.errors", this.solrAdmin.getModelErrors());
+        logger.debug("solr.model.errors done");
         vars.put("properties.residual", this.getResidualProperties("alfresco"));
+        logger.debug("solr.properties.residual done");
         vars.put("cluster.nodes", (long) this.clustering.getNumClusterMembers());
+        logger.debug("cluster.nodes done");
 
         return new JsonWriterResolution() {
             protected void writeJson(JSONWriter jsonWriter) throws JSONException {
@@ -127,8 +140,7 @@ public class Monitoring {
         };
     }
 
-    private long getChecksum(QName modelQName, ModelDefinition modelDefinition)
-    {
+    private long getChecksum(QName modelQName, ModelDefinition modelDefinition) {
         final CRC32 crc = new CRC32();
         final OutputStream stream = new OutputStream() {
             public void write(int b) throws IOException {
