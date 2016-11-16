e0122d04a7c9ea613d9b5467f32c4363bf14c83a
Willem Van den Eynde <wvandeneynde@xenit.eu>
2016-11-16 11:51:56 +0100

    Added GET method to Solr client

--- src/main/java/eu/xenit/care4alf/search/SolrClientImpl.java
+++ src/main/java/eu/xenit/care4alf/search/SolrClientImpl.java
@@ -9,6 +9,7 @@ import org.apache.commons.codec.net.URLCodec;
 import org.apache.commons.httpclient.*;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.params.HttpClientParams;
 import org.json.JSONException;
@@ -42,6 +43,11 @@ public class SolrClientImpl implements SolrClient {
     @Override
     public String postMessage(String url, Multimap<String, String> parameters, String body) throws IOException, EncoderException {
         return basePost(url, parameters, body);
+    }
+
+    @Override
+    public String get(String url, Multimap<String, String> parameters) throws IOException, EncoderException {
+        return this.baseGET(url, parameters);
     }
 
     private String basePost(String url, Multimap<String, String> parameters, String body) throws IOException, EncoderException {
@@ -103,6 +109,64 @@ public class SolrClientImpl implements SolrClient {
             return post.getResponseBodyAsString();
         } finally {
             post.releaseConnection();
+        }
+    }
+
+    private String baseGET(String url, Multimap<String, String> parameters) throws IOException, EncoderException {
+        HttpClientFactory httpClientFactory = (HttpClientFactory) (solrhttpClientFactory).getApplicationContext().getBean("solrHttpClientFactory");
+        final HttpClient httpClient = httpClientFactory.getHttpClient();
+        HttpClientParams params = httpClient.getParams();
+        params.setBooleanParameter(HttpClientParams.PREEMPTIVE_AUTHENTICATION, true);
+        httpClient.getState().setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT), new UsernamePasswordCredentials("admin", "admin"));
+
+        final URLCodec encoder = new URLCodec();
+        StringBuilder urlBuilder = new StringBuilder();
+        if (parameters == null) {
+            parameters = ArrayListMultimap.create();
+        }
+
+        for (Map.Entry<String, String> entry : parameters.entries()) {
+            if (urlBuilder.length() == 0) {
+                urlBuilder.append("?");
+            } else {
+                urlBuilder.append("&");
+            }
+            String value = entry.getValue();
+            logger.debug("Key/Value {}={}", entry.getKey(), entry.getValue());
+            if (value.indexOf('+') == -1) {
+                value = encoder.encode(value, "UTF-8");
+            }
+            urlBuilder.append(encoder.encode(entry.getKey())).append("=").append(value);
+        }
+        urlBuilder.insert(0, url);
+        logger.info("parameters {}", parameters);
+
+        final String uri = urlBuilder.toString();
+        logger.info("solr query: {}", uri);
+
+        GetMethod get = new GetMethod(uri);
+
+        try {
+            httpClient.executeMethod(get);
+
+            if (get.getStatusCode() == HttpStatus.SC_MOVED_PERMANENTLY || get.getStatusCode() == HttpStatus.SC_MOVED_TEMPORARILY) {
+                Header locationHeader = get.getResponseHeader("location");
+                if (locationHeader != null) {
+                    String redirectLocation = locationHeader.getValue();
+                    get.setURI(new URI(redirectLocation, true));
+                    httpClient.executeMethod(get);
+                }
+            }
+
+
+            if (get.getStatusCode() != HttpServletResponse.SC_OK) {
+                logger.error("HTTP error: " + get.getResponseBodyAsString());
+                throw new IOException("Request failed " + get.getStatusCode());
+            }
+
+            return get.getResponseBodyAsString();
+        } finally {
+            get.releaseConnection();
         }
     }
 
