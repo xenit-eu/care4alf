31fd727de8bab5fb96984f0f34b13879f9dfa84d
Younes Regaieg <younes.regaieg@xenit.eu>
2017-05-23 14:19:54 +0200

    ALFREDOPS-8: Introduce metric for last LDAP sync status

---
+++ src/main/java/eu/xenit/care4alf/monitoring/metric/LDAPSyncMetric.java
@@ -0,0 +1,50 @@
+package eu.xenit.care4alf.monitoring.metric;
+
+import eu.xenit.care4alf.monitoring.AbstractMonitoredSource;
+import org.alfresco.repo.security.sync.SyncStatus;
+import org.alfresco.service.cmr.attributes.AttributeService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yregaieg on 23.05.17.
+ */
+@Component
+public class LDAPSyncMetric extends AbstractMonitoredSource {
+    private final Logger logger = LoggerFactory.getLogger(LDAPSyncMetric.class);
+
+    private static final String STATUS_ATTRIBUTE = "STATUS";
+    public static final String ROOT_ATTRIBUTE_PATH = ".ChainingUserRegistrySynchronizer";
+
+    @Autowired
+    private AttributeService attributeService;
+
+    @Override
+    public Map<String, Long> getMonitoringMetrics() {
+        Map<String, Long> metrics = new HashMap<>();
+        String statusStr = (String) attributeService.getAttribute(ROOT_ATTRIBUTE_PATH, STATUS_ATTRIBUTE);
+        Long status;
+        if (SyncStatus.WAITING.toString().equals(statusStr)){
+            status = 1L;
+        }else if (SyncStatus.IN_PROGRESS.toString().equals(statusStr)){
+            status = 2L;
+        }else if (SyncStatus.COMPLETE.toString().equals(statusStr)){
+            status = 3L;
+        }else if (SyncStatus.COMPLETE_ERROR.toString().equals(statusStr)){
+            status = -1L;
+        }else{
+            status = 0L;
+        }
+
+        metrics.put("sync.ldap.status", status);
+        if (logger.isDebugEnabled()) {
+            logger.debug("LDAP sync status : " + status + " - " + statusStr);
+        }
+        return metrics;
+    }
+}
