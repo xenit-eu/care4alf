6ea32aec1225dcf01af30108d4574552b7fcbad2
Roel <roel.schevenels@xenit.eu>
2019-08-01 11:40:57 +0200

    ALFREDOPS-293 Formatting

--- c4a-impl/src/main/java/eu/xenit/care4alf/monitoring/AbstractMonitoredSource.java
+++ c4a-impl/src/main/java/eu/xenit/care4alf/monitoring/AbstractMonitoredSource.java
@@ -11,14 +11,11 @@ import org.springframework.stereotype.Component;
 import javax.annotation.PostConstruct;
 import java.util.List;
 
-/**
- * Created by willem on 5/10/17.
- */
 @Component
-public  abstract class AbstractMonitoredSource implements MonitoredSource, Task {
+public abstract class AbstractMonitoredSource implements MonitoredSource, Task {
     private final Logger logger = LoggerFactory.getLogger(AbstractMonitoredSource.class);
 
-    @Autowired()
+    @Autowired
     @Qualifier("global-properties")
     private java.util.Properties properties;
 
@@ -30,51 +27,54 @@ public  abstract class AbstractMonitoredSource implements MonitoredSource, Task 
     private List<MetricsShipper> allShippers;
 
     @PostConstruct
-    public void initMonitoredSourceConfig(){
+    public void initMonitoredSourceConfig() {
         this.shipperName = properties.getProperty("c4a.monitoring.shipper", "graphite");
 
         // Config to disable/enable a particular shipper
-        this.enabled = Boolean.parseBoolean(getMonitoringConfigProperty("enabled", shipperName, "false")) &&
-                Boolean.parseBoolean(getMonitoringConfigProperty("enabled", "metric." + this.getName(), "true"));
+        this.enabled = Boolean.parseBoolean(getMonitoringConfigProperty("enabled", shipperName, "false"))
+                && Boolean.parseBoolean(getMonitoringConfigProperty("enabled", "metric." + this.getName(), "true"));
 
-        if(!enabled) {
+        if (!enabled) {
             logger.warn("Metric {} is disabled!", this.getName());
             return;
         }
 
-        for (MetricsShipper ms : allShippers){
-            if (ms.getName().equals(shipperName)){
+        for (MetricsShipper ms : allShippers) {
+            if (ms.getName().equals(shipperName)) {
                 shipper = ms;
                 break;
             }
         }
-        if (shipper == null){
+        if (shipper == null) {
             logger.error("A metrics shipper was configured, but no implementation was found !");
             logger.error("Disabling monitoring...");
             this.enabled = false;
         }
     }
 
-    private String getMonitoringConfigProperty(String subKey, String key, String defaultValue){
-        return properties.getProperty("c4a.monitoring."+key+"."+subKey, properties.getProperty("c4a.monitoring."+subKey, defaultValue));
+    private String getMonitoringConfigProperty(String subKey, String key, String defaultValue) {
+        return properties.getProperty("c4a.monitoring." + key + "." + subKey,
+                properties.getProperty("c4a.monitoring." + subKey, defaultValue));
     }
 
     @Override
     public void execute() {
-        if(!enabled)
+        if (!enabled) {
             return;
+        }
 
         try {
             logger.debug("Fetching and sending metrics...");
             shipper.send(this.getMonitoringMetrics());
         } catch (Exception e) {
             logger.warn("Can't send metrics: " + this.shipper.toString());
-            if(logger.isDebugEnabled())
+            if (logger.isDebugEnabled()) {
                 e.printStackTrace();
+            }
         }
     }
 
-    public String getName(){
-        return this.getClass().getSimpleName().toLowerCase().replace("metrics","").replace("metric","");
+    public String getName() {
+        return this.getClass().getSimpleName().toLowerCase().replace("metrics", "").replace("metric", "");
     }
 }
