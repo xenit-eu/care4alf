1a370269a9d97732d94ad726d0d34f3a2fb4f4b4
Kenneth Piot <kenneth.piot@xenit.eu>
2019-08-01 12:10:08 +0200

    ALFREDOPS-291 Added symlink creation for c4a-test 5x to setup.bat

--- setup.bat
+++ setup.bat
@@ -23,3 +23,8 @@ mklink /J "c4a-impl/6x/src/main/kotlin-shared" "c4a-impl/src/main/kotlin"
 mklink /J "c4a-impl/6x/src/main/less" "c4a-impl/src/main/less"
 mklink /J "c4a-impl/6x/src/main/resources" "c4a-impl/src/main/resources"
 mklink /J "c4a-impl/6x/src/main/ts" "c4a-impl/src/main/ts"
+
+rmdir /s /q "c4a-test/5x/src/main/java"
+rmdir /s /q "c4a-test/5x/src/main/resources"
+mklink /J "c4a-test/5x/src/main/java" "c4a-test/src/main/java"
+mklink /J "c4a-test/5x/src/main/resources" "c4a-test/src/main/resources"
